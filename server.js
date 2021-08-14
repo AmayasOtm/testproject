fa7e2cf7c207212c7817a253a89d4c75f2b4755e
unknown <diavoboss@gmail.com>
2021-08-14 17:39:44 +0100

    added a database

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 const app = express()
 
+app.set('view engine','ejs')
+
+const database_link = "mongodb+srv://natomi:amayas@tut.pcvss.mongodb.net/node-tutorial?retryWrites=true&w=majority"
+
+mongoose.connect(database_link, {useNewUrlParser:true,useUnifiedTopology:true})
+    .then((result)=>{app.listen(3000,(err)=>{if(err)console.log('err')})})
+    .catch((err)=>{console.log(err)})
+
 app.get('/',(req,res) =>
 {
-    app.render('index.ejs');
-})
+    const blog = new Blog(
+        {
+            title:'title',snippet:'snippet',body:'body body'
+        }
+    )
+    blog.save()
+        .then((result) =>
+            {
+                res.send(result);
+            })
+        .catch((err)=>
+        {
+            console.log(err)
+        })
 
-app.listen(3000)
+})
